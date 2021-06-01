1d9052f3512204eaaa357c29734e21fd70d578b2
chris <191240002@smail.nju.edu.cn>
2021-06-01 21:17:43 +0800

    4-14-D

--- 4-14/4-14-D/main.cpp
+++ 4-14/4-14-D/main.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 #include <cassert>
 #define MAX_M 1003
-#define MAX_DIS 100000.0
+#define MAX_DIS 1000000.0
 #define PI 3.1415926
 using namespace std;
 
@@ -10,8 +10,8 @@ int T;
 int x,y,m;
 pair<double, double> points[MAX_M];
 double sx, sy;
-double delta = 0.99;
-double t_origin = 0.01;
+double delta = 0.999;
+double t_origin = 0.1;
 double t_min;
 double dis;
 
@@ -43,7 +43,7 @@ void sa(double t) {
             get_new_state(nx, ny, t);
         } while (!inside(nx, ny));
         double new_dis = find_min_dis(nx, ny);
-        double delta_e = dis - new_dis;
+        double delta_e = new_dis- dis;
         if (delta_e > 0 || (double)(rand() % RAND_MAX) / RAND_MAX < exp(delta_e / t)) {
             sx = nx;
             sy = ny;
@@ -69,6 +69,10 @@ int main() {
         dis = find_min_dis(sx, sy);
         t_min = t_origin * 1e-3 / (min(x,y)/2);
         sa(t_origin);
-        printf("%lf %lf", sx, sy);
+        int sx_10 = round(sx * 10);
+        int sy_10 = round(sy * 10);
+        double sx_out = double(sx_10) / 10.0;
+        double sy_out = double(sy_10) / 10.0;
+        printf("The safest point is (%.1lf, %.1lf).\n", sx_out, sy_out);
     }
 }
