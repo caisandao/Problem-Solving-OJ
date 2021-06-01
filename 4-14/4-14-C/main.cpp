094b5525239394144f67ab5c40f2e12c58451ec7
chris <191240002@smail.nju.edu.cn>
2021-06-01 09:22:02 +0800

    4-14-C

--- 4-14/4-14-C/main.cpp
+++ 4-14/4-14-C/main.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <cmath>
 #include <cassert>
-#define N 10000
 #define PI 3.1415926
 using namespace std;
 
-// 可调参数 t_origin, delta, t_min
+// 可调参数 t_origin, delta, t_min, N
 
 double a,b,c,d,e,f;
 double dx, dy, dz;
 double sx,sy,sz; // selected point
 double ans;
-double t_origin = 0.0001;
-double delta = 0.99999;
+double t_origin = 0.000001;
+double delta = 0.999997;
 double t_min;
 
 inline bool inside(double x, double y) {
@@ -21,7 +20,7 @@ inline bool inside(double x, double y) {
 
 inline double calculate_z(double x, double y) {
     double tmp = 1 - (x-d)*(x-d)/(a*a) - (y-e)*(y-e)/(b*b);
-    return f + c*sqrt(tmp);
+    return c*sqrt(tmp);
 }
 
 inline double calculate_dis(double x, double y, double z) {
@@ -32,8 +31,8 @@ inline double calculate_dis(double x, double y, double z) {
 }
 
 void get_new_state(double &nx, double &ny, double x, double y, double t) {
-    float angle = 2 * PI * (float(rand() % N) / N);
-    double r = sqrt(a*a+b*b) * t / t_origin;
+    float angle = 2 * PI * (float(rand() % RAND_MAX) / RAND_MAX);
+    double r = /*sqrt(a*a+b*b)*/a * t / t_origin;
     nx = x + r * cos(angle);
     ny = y + r * sin(angle);
 }
@@ -45,12 +44,11 @@ void sa(double T) {
             get_new_state(nx, ny, sx, sy, T);
         } while (!inside(nx, ny));
         nz = calculate_z(nx, ny);
-        double delta_e = ans - min(calculate_dis(nx,ny,nz), calculate_dis(nx,ny,-nz));
-        if (delta_e > 0 || (double(rand() % N) / N) < exp(delta_e/T)) {
+        double delta_e = ans - min(calculate_dis(nx,ny,f+nz), calculate_dis(nx,ny,f-nz));
+        if (delta_e > 0 || (double(rand() % RAND_MAX) / RAND_MAX) < exp(delta_e/T)) {
             sx = nx;
             sy = ny;
-            sz = nz;
-            ans = min(calculate_dis(nx,ny,nz), calculate_dis(nx,ny,-nz));
+            ans = min(calculate_dis(nx,ny,f+nz), calculate_dis(nx,ny,f-nz));
         }
         T = delta * T;
     }
@@ -60,13 +58,13 @@ int main() {
     scanf("%lf%lf%lf%lf%lf%lf", &a, &b, &c, &d, &e, &f);
     scanf("%lf%lf%lf", &dx, &dy, &dz);
     srand((unsigned)time(NULL));
-    sx = 2*a*(double(rand() % (N+1)) / N) - a + d;
-    int y_bound = sqrt(b*b-(b*b)/(a*a)*(sx-d)*(sx-d));
-    sy = 2*y_bound*(double(rand() % (N+1)) / N) - y_bound + e;
+    sx = 2*a*(double(rand() % RAND_MAX) / RAND_MAX) - a + d;
+    double y_bound = sqrt(b*b-(b*b)/(a*a)*(sx-d)*(sx-d));
+    sy = 2*y_bound*(double(rand() % RAND_MAX) / RAND_MAX) - y_bound + e;
     assert(inside(sx, sy));
     sz = calculate_z(sx, sy);
-    ans = min(calculate_dis(sx, sy, sz), calculate_dis(sx,sy,-sz));
-    t_min = t_origin * 1e-40 /sqrt(a*a+b*b);
+    ans = min(calculate_dis(sx, sy, f+sz), calculate_dis(sx,sy,f-sz));
+    t_min = t_origin * 1e-4 /(/*sqrt(a*a+b*b)*/a);
     sa(t_origin);
     printf("%lf\n", ans);
     return 0;
